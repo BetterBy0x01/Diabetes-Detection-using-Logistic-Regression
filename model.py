880d951f68b673dc8ed3df785a59835c59a61376
Ashutosh Singh Patel <2005017@kiit.ac.in>
2023-04-07 19:44:25 +0530

    Change in OurTeam.html

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-from PIL import Image
+# from sklearn.ensemble import 
+# from sklearn.model_selection import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('C:/Users/ronak/Desktop/flask/diabetes.csv')
@@ -10,7 +11,7 @@ X = df.iloc[:, 0:8].values
 Y = df.iloc[:, -1].values
 X_train, X_test, Y_train, Y_test = train_test_split(
     X, Y, test_size=0.25, random_state=0)
-lr = RandomForestClassifier()
+lr = LogisticRegression()
 lr.fit(X_train, Y_train)
 plt.plot(X, lr.predict(X))
 
