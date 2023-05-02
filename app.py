c6cdd675d01573a964532127b2f8a9b7a1e5383a
Ashutosh Singh Patel <2005017@kiit.ac.in>
2023-05-03 00:00:40 +0530

    Made some changes

--- app.py
+++ app.py
@@ -68,6 +68,5 @@ def predict():
             return render_template("alert.html")
 # app.run(debug = True)
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run()
