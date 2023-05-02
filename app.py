38d441e61a2f5fd5566a413c9a37bdeda8af5ade
Ashutosh Singh Patel <2005017@kiit.ac.in>
2023-05-02 23:52:20 +0530

    requirements.txt file changed

--- app.py
+++ app.py
@@ -66,4 +66,8 @@ def predict():
         
         else:
             return render_template("alert.html")
-app.run(debug = True)
+# app.run(debug = True)
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
